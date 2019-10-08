cc0c40790c1dd5859ca0c2d9dc4a8a2bd128c6fd
Tri Vuong <trivektor@gmail.com>
2019-10-08 02:04:24 -0700

    lots of cosmetics updates

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ import "babel-polyfill";
 import {get, isEmpty} from "lodash-es";
 
 const file = document.querySelector("#file");
-const SERVER_SIDE_URL = "http://localhost:4567";
+const loadingIndicator = document.querySelector("#loading-indicator");
+const SERVER_SIDE_URL = "http://b25fa54a.ngrok.io";
+
+mapkit.init({
+  authorizationCallback: function(done) {
+    fetch(`${SERVER_SIDE_URL}/services/jwt`)
+      .then((response) => response.text())
+      .then(done);
+  },
+  language: "en",
+});
 
 file.addEventListener("change", async (event) => {
+  loadingIndicator.style.visibility = "visible";
+
   const fd = new FormData;
 
   fd.append("file", event.target.files[0]);
@@ -26,20 +38,10 @@ file.addEventListener("change", async (event) => {
     if (latitude && longitude) {
       const locationCoordinate = new mapkit.Coordinate(latitude, longitude);
 
-      mapkit.init({
-        authorizationCallback: function(done) {
-          fetch(`${SERVER_SIDE_URL}/services/jwt`)
-            .then((response) => response.text())
-            .then(done);
-        },
-        language: "en",
-      });
-
       locationAnnotations.push(
         new MarkerAnnotation(locationCoordinate, {
-          color: "transparent",
+          color: "#FF4B2B",
           title: landmarkAnnotation.description,
-          glyphText: "🎃",
         })
       );
     }
@@ -49,4 +51,6 @@ file.addEventListener("change", async (event) => {
 
   const map = new mapkit.Map("map");
   map.showItems(locationAnnotations);
+
+  loadingIndicator.style.visibility = "hidden";
 });
