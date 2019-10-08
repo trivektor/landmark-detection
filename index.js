1cc85165a5762c9f6667a4df156d62104b54543f
Tri Vuong <trivektor@gmail.com>
2019-10-07 22:50:47 -0700

    update

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ import "babel-polyfill";
 import {get, isEmpty} from "lodash-es";
 
 const file = document.querySelector("#file");
+const SERVER_SIDE_URL = "http://localhost:4567";
 
 file.addEventListener("change", async (event) => {
   const fd = new FormData;
 
   fd.append("file", event.target.files[0]);
 
-  const landmarkAnnotation = await fetch("http://localhost:4567/upload-image", {
+  const landmarkAnnotations = await fetch(`${SERVER_SIDE_URL}/upload-image`, {
     method: "POST",
     body: fd,
     headers: {"Accept": "application/json"}
   })
   .then((response) => response.json())
-  .then((json) => get(json, "responses.0.landmarkAnnotations.0"));
+  .then((json) => get(json, "responses.0.landmarkAnnotations") || []);
 
-  if (landmarkAnnotation) {
+  const locationAnnotations = [];
+
+  landmarkAnnotations.forEach((landmarkAnnotation) => {
     const MarkerAnnotation = mapkit.MarkerAnnotation;
     const {latitude, longitude} = get(landmarkAnnotation, "locations.0.latLng") || {};
 
@@ -25,19 +28,25 @@ file.addEventListener("change", async (event) => {
 
       mapkit.init({
         authorizationCallback: function(done) {
-          fetch("http://localhost:4567/services/jwt")
+          fetch(`${SERVER_SIDE_URL}/services/jwt`)
             .then((response) => response.text())
             .then(done);
         },
         language: "en",
       });
 
-      const map = new mapkit.Map("map");
-      const locationAnnotation = new MarkerAnnotation(locationCoordinate, {
-        color: "red",
-        title: landmarkAnnotation.description,
-      });
-      map.showItems([locationAnnotation]);
+      locationAnnotations.push(
+        new MarkerAnnotation(locationCoordinate, {
+          color: "transparent",
+          title: landmarkAnnotation.description,
+          glyphText: "🎃",
+        })
+      );
     }
-  }
+
+    console.log({locationAnnotations});
+  });
+
+  const map = new mapkit.Map("map");
+  map.showItems(locationAnnotations);
 });
